b98ca06960d64c3598601245c4147c6be0055025
İbrahim Ethem Delemen <152957099+iedelemen@users.noreply.github.com>
2025-07-23 18:23:25 +0300

    added useful command list

---
+++ todo.sh
@@ -0,0 +1,19 @@
+k3d cluster create my-cluster --port 8888:30080
+	k3d cluster list
+	docker ps
+	kubectl cluster-info
+	
+kubectl create namespace argocd
+kubectl create namespace dev
+	kubectl get namespaces
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+	kubectl get pods -n argocd -w
+	kubectl get all -n argocd
+
+kubectl apply -f p3/configs/application.yaml -n argocd
+	kubectl get application -n argocd
+	kubectl get pods -n dev
+	kubectl get deployment,service -n dev
+
+curl http://localhost:8888
